4e9ddc45e707d2c66250f995a008498fbbc60ccc
faizyi <faizi13062000@gmail.com>
2024-11-02 09:02:48 +0500

    db

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import config from "./src/configs/server.js";
 import corsOptions from "./src/configs/cors.js";
 import connectToDB from "./src/db/db.js";
+import ProductRoutes from "./src/routes/products.js"
 const app = express();
 
 connectToDB();
@@ -17,6 +18,7 @@ app.use(cors(corsOptions));
 app.get("/", (req, res) => {
     res.send("hello server");
 });
+app.use("/", ProductRoutes);
 
 
 app.listen(config.appPort, () => {
---
+++ src/controllers/products.js
@@ -0,0 +1,14 @@
+import ProductModel from "../../src/models/product.js";
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await ProductModel.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Error fetching products" });
+    }
+}
+
+export {
+    getProducts
+}
