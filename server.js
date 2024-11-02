ad9bf6d793ff7f3c72affa8ce89f0c41ddbe4c69
faizyi <faizi13062000@gmail.com>
2024-11-02 09:08:20 +0500

    db again

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import connectToDB from "./src/db/db.js";
 import ProductRoutes from "./src/routes/products.js"
 const app = express();
 
-connectToDB();
+(async () => {
+    await connectToDB();
+  })();
 
 app.use(bodyParser.json({ limit: "50mb" })); // Increase limit for large files
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,22 @@
-import mongoose from 'mongoose';
-import config from '../configs/server.js';
+import mongoose from "mongoose";
+import config from "../configs/server.js";
+
+let isConnected = false; // Track connection status
+
 const connectToDB = async () => {
+  if (isConnected) {
+    return; // If already connected, skip connection
+  }
+
   try {
-    await mongoose
-      .connect(config.dbURI, {
-      })
-      .then(() => {
-        console.log("Connected to MongoDB database successfully.");
-      })
-      .catch((error) => {
-        console.log("Error connecting to MongoDB: ", error.message);
-      });
+    const db = await mongoose.connect(config.dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState; // 1 means connected
+    console.log("Connected to MongoDB database successfully.");
   } catch (error) {
-    console.log("Database connection error: ", error.message);
+    console.error("Error connecting to MongoDB:", error.message);
   }
 };
 
